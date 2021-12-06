ce1a21db0b951eac7b22263c929af0b142a65604
christycrt <christycrt@gmail.com>
2021-12-06 23:17:27 +0700

    [create] home page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <img
+        className="intro-mobile"
+        src="images/image-intro-mobile.jpg"
+        alt="intro-mobile"
+      />
+    </div>
+  );
+};
+
+export default Home;
