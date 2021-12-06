582ee7cd7406a855f814f56a7cb7a10f1ca1f322
christycrt <christycrt@gmail.com>
2021-12-06 23:17:51 +0700

    [create] navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="nav-bar container">
+      <img className="logo" src="images/logo.svg" alt="logo" />
+      <img
+        className="hamburger-menu"
+        src="images/icon-hamburger.svg"
+        alt="hamburger-menu"
+      />
+    </nav>
+  );
+};
+
+export default Navbar;
