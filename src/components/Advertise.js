0e413098f11a09c63802ab55b4a9b7384334fa8c
christycrt <christycrt@gmail.com>
2021-12-18 01:30:58 +0700

    [create] advertise component

---
+++ src/components/Advertise.js
@@ -0,0 +1,12 @@
+const Advertise = () => {
+  return (
+    <div className="advertise-wrapper">
+      <div className="advertise container">
+        <p className="work">Find out more about how we work</p>
+        <button className="btn">HOW WE WORK</button>
+      </div>
+    </div>
+  );
+};
+
+export default Advertise;
